c8b633e4a12a39ff8db91b02f1d54de706b02fa6
ankile <lars.ankile@gmail.com>
2023-11-02 19:26:03 -0400

    Add first log-likelihood objective experiment

--- auxiliary/mdp_solver.py
+++ auxiliary/mdp_solver.py
@@ -8,8 +8,15 @@ def value_iteration(env, delta=0.001):
     Q = np.zeros([env.state_space.n, env.action_space.n])
     pol = np.zeros([env.state_space.n, env.action_space.n])
 
-    if env.type == 'Maze':
-        eff_state_space = [i for i in range(env.state_space.n) if (i % env.width != 0) and (i % env.width != env.width - 1) and (i > env.width) and (i < env.width * (env.width - 1))]
+    if env.type == "Maze":
+        eff_state_space = [
+            i
+            for i in range(env.state_space.n)
+            if (i % env.width != 0)
+            and (i % env.width != env.width - 1)
+            and (i > env.width)
+            and (i < env.width * (env.width - 1))
+        ]
     else:
         eff_state_space = range(env.state_space.n)
     counter = 0
@@ -18,7 +25,10 @@ def value_iteration(env, delta=0.001):
         V_old = V.copy()
         for s in eff_state_space:
             for a in range(env.action_space.n):
-                Q[s, a] = env.get_reward(s) + env.gamma * np.dot(env.get_transition_probabilities(s, a), V[:])
+                Q[s, a] = env.get_reward(s) + env.gamma * np.dot(
+                    env.get_transition_probabilities(s, a), V[:]
+                )
+                print(Q)
             V[s] = max(Q[s, :])
         if max(np.abs(V_old - V)) < delta:
             for s in eff_state_space:
@@ -36,7 +46,10 @@ def policy_evaluation(env, policy, delta=0.0001):
         V_old = V.copy()
         for s in range(env.state_space.n):
             for a in range(env.action_space.n):
-                Q[s, a] = policy[s, a] * (env.get_reward(s) + env.gamma * np.dot(env.get_transition_probabilities(s, a), V[:]))
+                Q[s, a] = policy[s, a] * (
+                    env.get_reward(s)
+                    + env.gamma * np.dot(env.get_transition_probabilities(s, a), V[:])
+                )
             V[s] = np.sum(Q[s, :])
         if max(np.abs(V_old - V)) < delta:
             break
