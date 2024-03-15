0cf6d1b321deed65f55889143236500d3e622ae6
Paul Nitschke <pnitschke@g.harvard.edu>
2024-03-15 14:59:16 -0400

    got implicit diff to work

--- src/utils/visualization.py
+++ src/utils/visualization.py
@@ -47,7 +47,8 @@ from matplotlib.patches import Rectangle
 
 def plot_environment(reward_function, wall_states, start_state=(0, 0), ax=None,
                      annotate=True,
-                     colorbar=False):
+                     colorbar=False,
+                     title:str = "Environment"):
     # Assume the reward function is already reshaped to a 2D grid
     N, M = reward_function.shape
     # Identify wall states is the indixes into the
@@ -71,7 +72,7 @@ def plot_environment(reward_function, wall_states, start_state=(0, 0), ax=None,
         elif val != 0 and annotate:
             ax.text(j, i, f"{round(val, 1):g}", va="center", ha="center", color="white")
     ax.text(0,0, "S", va="center", ha="center", color="white")
-    ax.set_title(f"Environment")
+    ax.set_title(f"{title}")
 
 
 
